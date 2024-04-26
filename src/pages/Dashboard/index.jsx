7b5319339422d4fc507eac179fe60f398d58b650
SanHOWang <wang3harry@gmail.com>
2024-04-26 11:33:37 +0800

    add a small calender

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import EventInfoArea from "@/components/EventInfoArea";
-
+import Smallcalender from "@/components/Smallcalender";
 export default function Dashboard() {
   return (
     <>
+      <Smallcalender />
       <EventInfoArea />
     </>
   );
