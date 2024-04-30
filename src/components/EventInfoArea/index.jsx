eda17cb913b78c5d14dec674a2cf04b092fe4313
yeeway <yiwei.suuu@gmail.com>
2024-04-30 15:04:57 +0800

    Refactor EventInfoArea component to use EventIdContext in index.jsx

--- src/components/EventInfoArea/index.jsx
+++ src/components/EventInfoArea/index.jsx
@@ -1,15 +1,21 @@
-import { React, useState } from "react";
+import { React, useContext } from "react";
+import { EventIdContext } from "@/context/EventIdContext";
 import "./index.css";
+import eventData from "@/data/events.json";
+
 
 export default function EventInfoArea() {
-  const relativeEvents = ["S2O潑水音樂節", "台灣祭", "浮現祭", "共生音樂節"];
+  const eventId = useContext(EventIdContext);
+  // const handleClick = (id) => {
+  //   setEventId(id);
+  // };
 
   return (
     <section className="activity_info_section">
       <div className="activity_info_section_left">
         <div className="activity_intro">
-          <h3>大港開唱</h3>
-          <p>大港開唱（Megaport Festival）由台北「野台開唱」主辦團隊在2006年創立，是每年在高雄舉辦的音樂節慶，邀請許多人生閱歷豐富的知名業界前輩演出，也會與新一代的樂團或藝人合作演出，交融出世代之間的化學效應，近年逐漸成為台灣最具指標性的大型戶外音樂祭活動之一。</p>
+          <h3>{eventData[eventId].name}</h3>
+          <p>{eventData[eventId].intro}</p>
         </div>
         <div className="ai_signal">
           ai 訊號
@@ -18,8 +24,8 @@ export default function EventInfoArea() {
       <div className="activity_info_section_right">
         <div className="relative_events">
           <h3>相似活動</h3>
-          {relativeEvents.map((event, id) => {
-            return <button key={id}>{event}</button>;
+          {eventData[eventId].relatedEventsId.map((id) => {
+            return <button key={id}>{eventData[id].name}</button>;
           })}
         </div>
         <div className="history_records">
