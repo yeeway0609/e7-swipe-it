f5b01d0d43ecc188d1ff01ba7d72f71d5a60b58d
yeeway <yiwei.suuu@gmail.com>
2024-05-03 01:37:51 +0800

    Refactor css coding style with Stylelint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { React } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "@/pages/Dashboard";
 import About from "@/pages/About";
-import {Routes, Route} from "react-router-dom";
+import SideBar from "@/components/SideBar";
 
 export default function App() {
   return (
     <>
+      <SideBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path='/about' element={<About />} />
