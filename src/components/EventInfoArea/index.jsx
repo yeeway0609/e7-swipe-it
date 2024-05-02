4b4ce9697248cbe06d82fa6aee368498e59ec47d
yeeway <yiwei.suuu@gmail.com>
2024-05-03 01:06:31 +0800

    Modify css styling

--- src/components/EventInfoArea/index.jsx
+++ src/components/EventInfoArea/index.jsx
@@ -1,35 +1,74 @@
-import { React, useContext } from "react";
+import { React, useState, useContext } from "react";
 import { EventIdContext } from "@/context/EventIdContext";
+import { StarIcon as NotFavoriteIcon } from "@heroicons/react/24/outline";
+import { StarIcon as FavoriteIcon } from "@heroicons/react/24/solid";
 import "./index.css";
 import eventData from "@/data/events.json";
 
 
 export default function EventInfoArea() {
   const eventId = useContext(EventIdContext);
-  // const handleClick = (id) => {
-  //   setEventId(id);
-  // };
+  const [favorite, setFavorite] = useState(false);
+  const aiSignalTab = ["交通", "住宿", "票券", "飲食"];
+  const [aiTabActive, setAiTabActive] = useState("交通");
+  const historyTab = ["歷史1", "歷史2", "歷史3", "歷史4"];
+  const [historyTabActive, setHistoryTabActive] = useState("歷史1");
 
   return (
     <section className="activity_info_section">
       <div className="activity_info_section_left">
         <div className="activity_intro">
-          <h3>{eventData[eventId].name}</h3>
+          <div className="activity_intro_title">
+            <h3>{eventData[eventId].name}</h3>
+            {favorite
+              ? <FavoriteIcon className="star_icon" onClick={() => setFavorite(false)} />
+              : <NotFavoriteIcon className="star_icon" onClick={() => setFavorite(true)} />}
+          </div>
           <p>{eventData[eventId].intro}</p>
         </div>
         <div className="ai_signal">
-          ai 訊號
+          <ul className="tab_bar">
+            {aiSignalTab.map((tab) => {
+              return (
+                <li
+                  key={tab}
+                  onClick={() => setAiTabActive(tab)}
+                  className={tab == aiTabActive ? "active" : ""}
+                >
+                  {tab}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="ai_signal_info">
+            ai 訊號
+          </div>
         </div>
       </div>
       <div className="activity_info_section_right">
-        <div className="relative_events">
+        <div className="related_events">
           <h3>相似活動</h3>
           {eventData[eventId].relatedEventsId.map((id) => {
-            return <button key={id}>{eventData[id].name}</button>;
+            return <button key={id} className="related_event_badge">{eventData[id].name}</button>;
           })}
         </div>
         <div className="history_records">
-          <h3>歷史紀錄</h3>
+          <ul className="tab_bar">
+            {historyTab.map((tab) => {
+              return (
+                <li
+                  key={tab}
+                  onClick={() => setHistoryTabActive(tab)}
+                  className={tab == historyTabActive ? "active" : ""}
+                >
+                  {tab}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="history_records_info">
+            歷史紀錄
+          </div>
         </div>
       </div>
     </section>
