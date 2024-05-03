1fe8759e2358bd118f0b766c66ecc1272ef9c3ab
yeeway <yiwei.suuu@gmail.com>
2024-05-04 00:10:22 +0800

    Add SearchPanel component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,7 @@
 import "./style.css";
 import React from "react";
 import EventInfoArea from "@/components/EventInfoArea";
+import SearchPanel from "@/components/SearchPanel";
 import SmallCalendar from "@/components/SmallCalendar";
 import BigCalendar from "@/components/BigCalendar";
 
@@ -8,7 +9,10 @@ export default function Dashboard() {
   return (
     <div>
       <div className="calendar-area">
-        <SmallCalendar />
+        <div className="calendar-area-left">
+          <SearchPanel />
+          <SmallCalendar />
+        </div>
         <BigCalendar/>
       </div>
       <EventInfoArea />
