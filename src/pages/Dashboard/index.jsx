d3c76e6bc5ada6dce7da9ae2d56761e3703f5cb2
yeeway <yiwei.suuu@gmail.com>
2024-05-03 23:24:58 +0800

    Refactor SmallCalendar component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import EventInfoArea from "@/components/EventInfoArea";
 import SmallCalendar from "@/components/SmallCalendar";
 import BigCalendar from "@/components/BigCalendar";
-import SmallcalendarSingle from "@/components/SmallcalendarSingle";
 
 export default function Dashboard() {
   return (
     <div>
       <div className="calendar-area">
-        {/* <SmallCalendar /> */}
-        <SmallcalendarSingle/>
+        <SmallCalendar />
         <BigCalendar/>
       </div>
       <EventInfoArea />
