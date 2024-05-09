a50c4caf13516516308eeb832b4e2bcf66fec8a5
yeeway <yiwei.suuu@gmail.com>
2024-05-09 13:25:28 +0800

    chore: Update heat component CSS

--- src/components/heat/index.jsx
+++ src/components/heat/index.jsx
@@ -4,18 +4,18 @@ import Taiwan from '@svg-maps/taiwan.main';
 import './index.css';
 
 class HeatMapComponent extends React.Component {
-    constructor(props) {
-      super(props);
-      console.log(this.props); // => 如果前面兩個參數忘記加，這裡會無法存取
-      console.log('Title created');
-    }
-  
-    render() {
-        return (
-            <CheckboxSVGMap map={Taiwan}/>
-        );
-    }
+  constructor(props) {
+    super(props);
+    console.log(this.props); // => 如果前面兩個參數忘記加，這裡會無法存取
+    console.log('Title created');
   }
-  
 
-export default HeatMapComponent
+  render() {
+    return (
+      <CheckboxSVGMap map={Taiwan}/>
+    );
+  }
+}
+
+
+export default HeatMapComponent;
