c6a22334b70beb9bd3d3ec269dddce4bd206ee67
yeeway <yiwei.suuu@gmail.com>
2024-05-09 14:28:02 +0800

    chore: Remove unused BigCalendar components

--- src/components/Bigcalendar/index.jsx
+++ src/components/Bigcalendar/index.jsx
@@ -1,38 +1,13 @@
-import "./styles.css";
-import React, { useState } from "react";
-import Calendar from "./Calendar";
-import Details from "./Details";
+import "./style.css";
+import React from "react";
 
 export default function BigCalendar() {
-  const [showDetails, setShowDetails] = useState(false);
-  const [data, setData] = useState(null);
-
-  const showDetailsHandle = (dayStr) => {
-    setData(dayStr);
-    setShowDetails(true);
-  };
-
   return (
-    <div className="App">
-
-      {/* <h2 id="w1">總攬</h2> */}
-      {/* <h2 id="w2">收藏</h2>
-      <h2 id="w3">活動匯入</h2> */}
-      {/* <h1>Week View Calendar with react</h1> */}
-      <br />
-      {/* <h2>Example</h2> */}
-      <Calendar showDetailsHandle={showDetailsHandle} />
-      {/* <br /> */}
-      {showDetails && <Details data={data} />}
+    <div>
     </div>
   );
 }
 
-/**
- * Follow this tutorial https://medium.com/@moodydev/create-a-custom-calendar-in-react-3df1bfd0b728
- * and customised by TTNT
- * date-fns doc: https://date-fns.org/v2.21.1/docs
- */
 
 
 
