179153624edca04b04d33736c9ec73165fb1762d
SanHOWang <wang3harry@gmail.com>
2024-06-20 19:03:29 +0800

    eventinfo

--- src/components/EventInfoArea/index.jsx
+++ src/components/EventInfoArea/index.jsx
@@ -6,6 +6,86 @@ import { StarIcon as FavoriteIcon } from "@heroicons/react/24/solid";
 import CanvasJSReact from "@canvasjs/react-charts";
 import PieChartData from "@/data/PieChartData.json";
 import RightChartData from "@/data/RightChartData.json";
+// G1
+import member1_1 from "@/img/line_chart/G1/1-left.png";
+import member1_2 from "@/img/line_chart/G1/1-up.png";
+import member1_3 from "@/img/line_chart/G1/1-down.png";
+
+// G2
+import member2_1 from "@/img/line_chart/G2/2-left.png";
+import member2_2 from "@/img/line_chart/G2/2-up.png";
+import member2_3 from "@/img/line_chart/G2/2-down.png";
+
+// G3
+import member3_1 from "@/img/line_chart/G3/3-left.png";
+import member3_2 from "@/img/line_chart/G3/3-up.png";
+import member3_3 from "@/img/line_chart/G3/3-down.png";
+
+// G4
+import member4_1 from "@/img/line_chart/G4/4-left.png";
+import member4_2 from "@/img/line_chart/G4/4-up.png";
+import member4_3 from "@/img/line_chart/G4/4-down.png";
+
+// G5
+import member5_1 from "@/img/line_chart/G5/5-left.png";
+import member5_2 from "@/img/line_chart/G5/5-up.png";
+import member5_3 from "@/img/line_chart/G5/5-down.png";
+
+// G6
+import member6_1 from "@/img/line_chart/G6/6-left.png";
+import member6_2 from "@/img/line_chart/G6/6-up.png";
+import member6_3 from "@/img/line_chart/G6/6-down.png";
+
+// G7
+import member7_1 from "@/img/line_chart/G7/7-left.png";
+import member7_2 from "@/img/line_chart/G7/7-up.png";
+import member7_3 from "@/img/line_chart/G7/7-down.png";
+
+// G8
+import member8_1 from "@/img/line_chart/G8/8-left.png";
+import member8_2 from "@/img/line_chart/G8/8-up.png";
+import member8_3 from "@/img/line_chart/G8/8-down.png";
+
+// G9
+import member9_1 from "@/img/line_chart/G9/9-left.png";
+import member9_2 from "@/img/line_chart/G9/9-up.png";
+import member9_3 from "@/img/line_chart/G9/9-down.png";
+
+// G10
+import member10_1 from "@/img/line_chart/G10/10-left.png";
+import member10_2 from "@/img/line_chart/G10/10-up.png";
+import member10_3 from "@/img/line_chart/G10/10-down.png";
+
+// G11
+import member11_1 from "@/img/line_chart/G11/11-left.png";
+import member11_2 from "@/img/line_chart/G11/11-up.png";
+import member11_3 from "@/img/line_chart/G11/11-down.png";
+
+// G12
+import member12_1 from "@/img/line_chart/G12/12-left.png";
+import member12_2 from "@/img/line_chart/G12/12-up.png";
+import member12_3 from "@/img/line_chart/G12/12-down.png";
+
+// G13
+import member13_1 from "@/img/line_chart/G13/13-left.png";
+import member13_2 from "@/img/line_chart/G13/13-up.png";
+import member13_3 from "@/img/line_chart/G13/13-down.png";
+
+// G14
+import member14_1 from "@/img/line_chart/G14/14-left.png";
+import member14_2 from "@/img/line_chart/G14/14-up.png";
+import member14_3 from "@/img/line_chart/G14/14-down.png";
+
+// G15
+import member15_1 from "@/img/line_chart/G15/15-left.png";
+import member15_2 from "@/img/line_chart/G15/15-up.png";
+import member15_3 from "@/img/line_chart/G15/15-down.png";
+
+// G16
+import member16_1 from "@/img/line_chart/G16/16-left.png";
+import member16_2 from "@/img/line_chart/G16/16-up.png";
+import member16_3 from "@/img/line_chart/G16/16-down.png";
+
 
 export default function EventInfoArea() {
   const {eventId, setEventId} = useContext(EventIdContext);
@@ -24,8 +104,11 @@ export default function EventInfoArea() {
 
   async function handleCurrentEvent() {
     const data = await fetchEventByID(eventId);
+    if (data) {
+      data.top5_products = JSON.parse(data.top5_products); // 解析 top5_products
+      data.news_trend = JSON.parse(data.news_trend); // 解析 news_trend
+    }
     setCurrentEvent(data);
-
     if (data && data.related_events_id) {
       const relatedEventsIds = JSON.parse(data.related_events_id);
       const relatedEventsPromises = relatedEventsIds.map(id => fetchEventByID(id));
@@ -41,96 +124,129 @@ export default function EventInfoArea() {
   const [favorite, setFavorite] = useState(false);
   const aiSignalTab = ["交通", "住宿", "票券", "飲食"];
   const [aiTabActive, setAiTabActive] = useState("交通");
-  const historyTab = ["歷史1", "歷史2", "歷史3", "歷史4"];
-  const [historyTabActive, setHistoryTabActive] = useState("歷史1");
+  const historyTab = ["2023", "2022", "2021", "2020"];
+  const [historyTabActive, setHistoryTabActive] = useState("2023");
+  const [selectedProductIndex, setSelectedProductIndex] = useState(0);
 
   var CanvasJS = CanvasJSReact.CanvasJS;
   var CanvasJSChart = CanvasJSReact.CanvasJSChart;
   var CanvasJSChartRight = CanvasJSReact.CanvasJSChart;
-  const options = {
+  const left_pie_options = {
     animationEnabled: true,
     interactivityEnabled: true,
     highlightEnabled: true,
     explodeOnClick: true,
-    colorSet:"redShades",
-    backgroundColor: "#e0e0e0",
-    width:200,
-    height:200,
+    colorSet: "redShades",
+    backgroundColor: "#ffffff",
+    width: 250,
+    height: 250,
     data: [{
       type: "pie",
       startAngle: 0,
       indexLabel: "{name}",
       indexLabelPlacement: "inside",
       indexLabelFontSize: 16,
-      dataPoints:PieChartData
+      indexLabelFontColor: "#ffffff",
+      dataPoints: getLeftPieData(aiTabActive, eventId)
     }]
   };
 
-  const Line_Chart = {
-    animationEnabled: true,
-    interactivityEnabled: true,
-    highlightEnabled: true,
-    explodeOnClick: true,
-    backgroundColor: "#e0e0e0",
-    width:400,
-    height:200,
-    data: [{
-      type: "line",
-      lineColor: "red",
-      indexLabelFontSize: 16,
-      dataPoints: [
-        { x: new Date(2012, 0, 1), y: 450 },
-        { x: new Date(2012, 1, 1), y: 414 },
-        { x: new Date(2012, 2, 1), y: 520 },
-        { x: new Date(2012, 3, 1), y: 460 },
-        { x: new Date(2012, 4, 1), y: 450 },
-        { x: new Date(2012, 5, 1), y: 500 },
-        { x: new Date(2012, 6, 1), y: 480 },
-        { x: new Date(2012, 7, 1), y: 480 },
-        { x: new Date(2012, 8, 1), y: 410 },
-        { x: new Date(2012, 9, 1), y: 500 },
-        { x: new Date(2012, 10, 1), y: 480 },
-        { x: new Date(2012, 11, 1), y: 510 }
-      ]
-    }]
-  };
+  function getLeftPieData(tab, eventId) {
+    let eventIdInt = parseInt(eventId); 
+    let remainder = eventIdInt % 4; 
+    console.log("eventId:", eventId, "remainder:", remainder);
+  
+    switch (remainder) {
+      case 0:
+        switch (tab) {
+          case "交通":
+            return PieChartData.id1.transportation ;
+          case "住宿":
+            return PieChartData.id1.accommodation ;
+          case "票券":
+            return PieChartData.id1.tickets ;
+          case "飲食":
+            return PieChartData.id1.dining ;
+          default:
+            return [];
+        }
+      case 1:
+        switch (tab) {
+          case "交通":
+            return PieChartData.id2.transportation ;
+          case "住宿":
+            return PieChartData.id2.accommodation ;
+          case "票券":
+            return PieChartData.id2.tickets ;
+          case "飲食":
+            return PieChartData.id2.dining ;
+          default:
+            return [];
+        }
+      case 2:
+        switch (tab) {
+          case "交通":
+            return PieChartData.id3.transportation ;
+          case "住宿":
+            return PieChartData.id3.accommodation ;
+          case "票券":
+            return PieChartData.id3.tickets ;
+          case "飲食":
+            return PieChartData.id3.dining ;
+          default:
+            return [];
+        }
+      case 3:
+        switch (tab) {
+          case "交通":
+            return PieChartData.id4.transportation ;
+          case "住宿":
+            return PieChartData.id4.accommodation ;
+          case "票券":
+            return PieChartData.id4.tickets ;
+          case "飲食":
+            return PieChartData.id4.dining ;
+          default:
+            return [];
+        }
+      default:
+        return [];
+    }
+  }
+  
 
-  const optionss = {
-    //exportEnabled: true,
+  const right_pie_options = {
     animationEnabled: true,
     interactivityEnabled: true,
     colorSet:"redShades",
-    // title: {
-    //   text: "歷史紀錄~"
-    // },
-
+    width:250,
+    height:250,
     data: [{
       type: "pie",
       startAngle: 150,
       indexLabel: "{2023}",
       indexLabelPlacement: "inside",
       indexLabelFontSize: 16,
-      width: 30 ,
+      indexLabelFontColor: "#ffffff",
       dataPoints:RightChartData //json
     }]
-  };
+  }
 
   CanvasJS.addColorSet("redShades",
     [//colorSet Array
-      "#FF0000", // Red
-      "#FF6347", // Tomato
-      "#FF4500", // OrangeRed
-      "#DC143C", // Crimson
-      "#B22222", // FireBrick
-      "#8B0000", // DarkRed
-      "#A52A2A", // Brown
-      "#D2691E", // Chocolate
-      "#CD5C5C", // IndianRed
-      "#F08080"  // LightCoral
+      "rgba(201, 2, 15, 1)", // Red
+      "rgba(201, 2, 15, 0.85)", // Tomato
+      "rgba(201, 2, 15, 0.6)", // OrangeRed
+      "rgba(201, 2, 15, 0.5)", // Crimson
+      "rgba(201, 2, 15, 0.4)", // FireBrick
+      "rgba(201, 2, 15, 0.3)", // DarkRed
     ]);
 
+    function handleProductClick(productIndex) {
+      setSelectedProductIndex(productIndex === selectedProductIndex ? null : productIndex);
+    }
   return (
-    <section className="activity-info-section">
+    <div className="activity-info-section">
       <div className="activity-info-section-left">
         <div className="activity-intro">
           {favorite
@@ -152,18 +268,36 @@ export default function EventInfoArea() {
               );
             })}
           </ul>
-          <div className="ai-signal-info-right">
-            <div className="container">
-              <p>AI訊號推薦</p>
-              <CanvasJSChart className="chart" options = {options}/>
-              <p>商品訊號</p>
+          <div className="ai-signal-info-left">
+            <div className="AI-top">
+              <div className="AI-recommend">
+              <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+              <h3>&nbsp;&nbsp;&nbsp;&nbsp;AI訊號推薦</h3>
+              <div className="left-pie-chart">
+                <CanvasJSChart options = {left_pie_options}/>
+              </div>
+              <img src={member1_1} alt="G1 1-left" />
+              </div>
+              <div className="product-signal">
+              <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+              <h3>商品訊號</h3>
               <p>過去5年內當 高鐵 發生hike訊號時，在4天後 飯店 也會發生hike訊號，並延續56天。</p>
-              <CanvasJSChart className="chart" options = {Line_Chart}/>
-              <p>新聞熱搜關鍵字</p>
-              <p>新聞連結</p>
+              </div>
             </div>
+            <div className="AI-down"></div>
           </div>
         </div>
+        <div className="news-signal">
+              <h3>新聞熱搜關鍵字</h3>
+              <div className="news-keywords">
+                {currentEvent?.news_trend?.map((keyword, index) => (
+                  <div key={index} className="news-keyword">
+                    <p>{keyword}</p>
+                  </div>
+                ))}
+              </div>
+        </div>
+
       </div>
       <div className="activity-info-section-right">
         <div className="related-events">
@@ -195,13 +329,36 @@ export default function EventInfoArea() {
             })}
           </ul>
           <div className="history-records-info">
-            <h3>歷史紀錄</h3>
-            <div className="right_pie">
-              <CanvasJSChartRight options = {optionss}/>
+          <div className="history-records-info-top">
+            <div className="history-records-pie">
+              <h3>歷史紀錄</h3>
+              <div className="right_pie">
+                <CanvasJSChartRight options = {right_pie_options}/>
+              </div>
             </div>
+            <div className="history-records-top5">
+              <h3>前五名相關商品</h3>
+              <div className="top5-products"style={{ display: "flex" , alignItems: "center", flexDirection: "column"}}>
+                  {currentEvent?.top5_products?.map((product, index) => (
+                <div
+                  key={index}
+                  className="product-box"
+                  style={{
+                    backgroundColor: selectedProductIndex === index ? "#C9020F" : "#f0f0f0", 
+                    color: selectedProductIndex === index ? "#ffffff" : "#000000",
+                    cursor: "pointer"
+                  }}
+                  onClick={() => handleProductClick(index)} 
+                >
+                  <p>{product}</p>
+                </div>
+              ))}
+            </div>
+            </div>
+          </div>
           </div >
         </div>
       </div>
-    </section>
+    </div>
   );
 }
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -31,7 +31,9 @@ export default function Dashboard() {
         </div>
         <BigCalendar/>
       </div>
-      <EventInfoArea />
+      <div className="event-Info-Area">
+        <EventInfoArea />
+      </div>
     </div>
   );
 }
